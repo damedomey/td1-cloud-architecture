acba0b8bba568d1c33589ba56e74d42abcd7e8ab
AMEDOMEY Roméo David <davidromeoamedomey@yahoo.com>
2023-09-12 11:01:51 +0200

    feat: minimal configuration to show hello world on route /

---
+++ README.md
@@ -0,0 +1,4 @@
+# TD1 cloud architecture
+
+The goal of this project is to deploy a king of solution as PaaS, CaaS and FaaS in clever cloud.
+The full description of project is available in **subject.pdf**
---
+++ src/test/java/com/unice/politech/cloud/Td1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unice.politech.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
